fd8f7723c33743472c05e207abb68e1335f1a90b
xingliuchao <77254513+xingliuchao@users.noreply.github.com>
2021-01-25 09:36:46 +0800

    Update jd_sgmh.js

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -12,6 +12,7 @@ const inviteCodes = [
   'T0225KkcRh4bowCFIR2gwqMMfQCjVWmIaW5kRrbA@T019-aknAFRllhyoQlyI46gCjVWmIaW5kRrbA'
 ];
 const randomCount = $.isNode() ? 20 : 5;
+const notify = $.isNode() ? require('./sendNotify') : '';
 let merge = {}
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
